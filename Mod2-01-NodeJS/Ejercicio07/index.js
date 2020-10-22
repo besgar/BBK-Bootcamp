fdddcc8e229ecd4cf4979848a6dcfee62237bef1
besgar <elenabestilleiro@gmail.com>
2020-10-22 16:29:26 +0200

    Mod2-01-Ejercicio 7

---
+++ Mod2-01-NodeJS/Ejercicio07/index.js
@@ -0,0 +1,7 @@
+const numerosConsecutivos = require("./array.js");
+let numerosConsecu=require("./array.js");
+let aleatorio=require("./aleatorio.js");
+console.log(aleatorio);
+
+let array=numerosConsecu(aleatorio);
+console.log(array);
