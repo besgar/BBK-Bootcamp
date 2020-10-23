c25099a5ec51f9c8cde760729a3b34077bad585d
besgar <elenabestilleiro@gmail.com>
2020-10-23 16:38:45 +0200

    Mod2-02 Ejercicio 2 Faltaba parseInt

--- Mod2-02-Introduccion Express/Ejercicio02/index.js
+++ Mod2-02-Introduccion Express/Ejercicio02/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/:numero", function(request, response){
-    let numero=request.params.numero;
+    let numero=parseInt(request.params.numero);
     let aleatorio=Math.floor(Math.random() * parseInt(numero - 0) + 0);
 
     response.send(`Número aleatorio entre 0 y ${numero}: ${aleatorio}`);
